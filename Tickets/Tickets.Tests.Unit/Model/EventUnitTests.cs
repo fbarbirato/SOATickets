8d599f3f81851af80bceb74d170a6e017b8f5b55
Felipe Barbirato <fbarbirato@gmail.com>
2014-11-05 23:54:21 -0200

    Unit tests for AvailableAllocation in class Event, using NBuilder

--- Tickets/Tickets.Tests.Unit/Model/EventUnitTests.cs
+++ Tickets/Tickets.Tests.Unit/Model/EventUnitTests.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tickets.Model;
+using FizzWare.NBuilder;
 
 namespace Tickets.Tests.Unit.Model
 {
     [TestClass]
     public class EventUnitTests
     {
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
-        //}
+        [TestMethod]
+        public void AvailableAllocation_100Allocation10PurchasedAnd15ReservedWith10StillActive_Returns80TicketsAvailable()
+        {
+            //arrange
+            var Event = new Event
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Event",
+                Allocation = 100,
+                PurchasedTickets = Builder<TicketPurchase>.CreateListOfSize(2)
+                                        .All()
+                                        .With(t => t.TicketQuantity = 5)
+                                        .Build().ToList(),
+                ReservedTickets = Builder<TicketReservation>.CreateListOfSize(3)
+                                        .All()
+                                        .With(r => r.TicketQuantity = 5)
+                                        .TheFirst(2)
+                                        .With(r => r.ExpiryTime = DateTime.Now.AddHours(1))
+                                        .And(r => r.HasBeenRedeemed = false)
+                                        .TheNext(1)
+                                        .With(r => r.ExpiryTime = DateTime.Now.AddHours(-1))
+                                        .And(r => r.HasBeenRedeemed = true)
+                                        .Build().ToList()
+            };
+
+            //act
+            int result = Event.AvailableAllocation();
+
+            //assert
+            Assert.AreEqual(80, result);
+        }
     }
 }
