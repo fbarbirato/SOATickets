7844a68b6eb866543dc3edd90212c1096d0fea54
Felipe Barbirato <fbarbirato@gmail.com>
2014-11-06 00:05:45 -0200

    Unit test for TicketPurchaseFactory

---
+++ Tickets/Tickets.Tests.Unit/Model/TicketPurchaseFactoryUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tickets.Model;
+
+namespace Tickets.Tests.Unit.Model
+{
+    [TestClass]
+    public class TicketPurchaseFactoryUnitTests
+    {
+        [TestMethod]
+        public void CreateTicket_PassingEventAndTicketQuantity_CreatesTicketPurchase()
+        {
+            //arrange
+            var Event = new Event
+            {
+                Id = Guid.NewGuid()
+            };
+            var ticketQuantity = 10;
+
+            //act
+            var createdTicketPurchase = TicketPurchaseFactory.CreateTicket(Event, ticketQuantity);
+
+            //assert
+            Assert.IsInstanceOfType(createdTicketPurchase, typeof(TicketPurchase));
+            Assert.AreEqual(Event.Id, createdTicketPurchase.Event.Id);
+            Assert.AreEqual(ticketQuantity, createdTicketPurchase.TicketQuantity);
+        }
+    }
+}
