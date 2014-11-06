dde335ec5700b2ab63c88938e175046163465178
Felipe Barbirato <fbarbirato@gmail.com>
2014-11-06 00:12:26 -0200

    Unit test for TicketReservationFactory

---
+++ Tickets/Tickets.Tests.Unit/Model/TicketReservationFactoryUnitTestscs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tickets.Model;
+
+namespace Tickets.Tests.Unit.Model
+{
+    [TestClass]
+    public class TicketReservationFactoryUnitTestscs
+    {
+        [TestMethod]
+        public void CreateReservation_PassingEventAndTicketQuantity_CreatesTicketReservation()
+        {
+            //arrange
+            var Event = new Event
+            {
+                Id = Guid.NewGuid()
+            };
+            var ticketQuantity = 10;
+
+            //act
+            var createdTicketReservation = TicketReservationFactory.CreateReservation(Event, ticketQuantity);
+
+            //assert
+            Assert.IsInstanceOfType(createdTicketReservation, typeof(TicketReservation));
+            Assert.AreEqual(Event.Id, createdTicketReservation.Event.Id);
+            Assert.AreEqual(ticketQuantity, createdTicketReservation.TicketQuantity);
+        }
+    }
+}
