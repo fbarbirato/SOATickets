7fef22d775e6f4fecf99099b6f30cfe35e494152
Felipe Barbirato <fbarbirato@gmail.com>
2015-05-19 22:54:50 -0300

    Adding ITicketService

---
+++ Tickets/Tickets.Contracts/ITicketService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Tickets.DataContract;
+
+namespace Tickets.Contracts
+{
+    [ServiceContract(Namespace = "http://ASPPatterns.Chap6.EventTickets/")]
+    public interface ITicketService
+    {
+        [OperationContract()]
+        ReserveTicketResponse ReserveTicket(ReserveTicketRequest reserveTicketRequest);
+
+        [OperationContract()]
+        PurchaseTicketResponse PurchaseTicket(PurchaseTicketRequest purchaseTicketRequest);
+    }
+}
